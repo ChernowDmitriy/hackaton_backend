a83b9eedb8ef58e51d7cf234a6d50f71ffe0e8ff
Dmitriy Chernow <dmitriychernow2000@gmail.com>
2024-06-13 20:11:16 +0300

    removed taskiq

--- core/lifetime.py
+++ core/lifetime.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from core.services.rabbit.lifetime import init_rabbit, shutdown_rabbit
 from core.settings import get_settings
-from core.tkq import broker
 
 
 def _setup_db(app: FastAPI) -> None:  # pragma: no cover
@@ -44,8 +43,6 @@ def register_startup_event(
     @app.on_event("startup")
     async def _startup() -> None:  # noqa: WPS430
         app.middleware_stack = None
-        if not broker.is_worker_process:
-            await broker.startup()
         _setup_db(app)
         init_rabbit(app)
         app.middleware_stack = app.build_middleware_stack()
@@ -67,8 +64,6 @@ def register_shutdown_event(
     @app.on_event("shutdown")
     async def _shutdown() -> None:  # noqa: WPS430
         await app.state.db_engine.dispose()
-        if not broker.is_worker_process:
-            await broker.shutdown()
         await shutdown_rabbit(app)
         pass  # noqa: WPS420
 
--- src/backend/web/api/file/views.py
+++ src/backend/web/api/file/views.py
@@ -1,31 +1,26 @@
+import asyncio
 
 import aiofiles
 
 from fastapi import APIRouter, UploadFile, File
 
-from core.tkq import broker
 
 router = APIRouter()
 
 
-@broker.task
-async def process_file(file_path: str):
-    # Process the file here (e.g., read the file, process data)
-    print('=' * 254)
-    print(f"Processing file: {file_path}")
-
-
-@router.post("",
+@router.post("/test",
              description="Загрузить датасет")
 async def upload_file(
         file: UploadFile = File(...),
 ):
     # Save the uploaded file to disk
-    async with aiofiles.open(f"/tmp/{file.filename}", 'wb') as out_file:
-        content = await file.read()  # async read
-        await out_file.write(content)  # async write
+    # async with aiofiles.open(f"/tmp/{file.filename}", 'wb') as out_file:
+    #     content = await file.read()  # async read
+    #     await out_file.write(content)  # async write
 
     # Enqueue the task to process the file
-    await process_file.kiq(f"/tmp/{file.filename}")
-
+    # await process_file.kiq(f"/tmp/{file.filename}")
+    task = await add_one.kiq(1)
+    # Wait for the result.
+    result = await task.wait_result(timeout=2)
     return {"filename": file.filename, "status": "File uploaded and processing started"}
